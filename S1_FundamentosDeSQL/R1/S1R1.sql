21ce745e622d97f6180aca5390beb43683671a05
Paulo <phjdo@hotmail.com>
2020-11-01 20:18:56 -0600

    Sesión 1, Reto 1 agregado

---
+++ S1_FundamentosDeSQL/R1/S1R1.sql
@@ -0,0 +1,14 @@
+SHOW DATABASES;
+USE tienda;
+
+SHOW TABLES;
+
+# Usando la base de datos tienda, muestra:
+# 1. Descripción de la tabla articulo
+DESCRIBE articulo;
+
+# 2. Descripción de la tabla puesto
+DESCRIBE puesto;
+
+# 3. Descripción de la tabla venta
+DESCRIBE venta;
